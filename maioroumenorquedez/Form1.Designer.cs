c4a959424fe8fe0570212905d5376f118790ebfd
PAULO FRANCA <Paulo.franca@senacsp.edu.br>
2025-03-18 13:59:23 -0300

    exerciciosprogramadordesistemas

---
+++ maioroumenorquedez/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace maioroumenorquedez
+{
+    partial class frmMaiorouMenorqueDez
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaiorouMenorqueDez = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMaiorouMenorqueDez
+            // 
+            this.txtMaiorouMenorqueDez.Location = new System.Drawing.Point(252, 24);
+            this.txtMaiorouMenorqueDez.Name = "txtMaiorouMenorqueDez";
+            this.txtMaiorouMenorqueDez.Size = new System.Drawing.Size(203, 20);
+            this.txtMaiorouMenorqueDez.TabIndex = 0;
+            this.txtMaiorouMenorqueDez.Text = "Valor";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(298, 70);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(114, 46);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // frmMaiorouMenorqueDez
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtMaiorouMenorqueDez);
+            this.Name = "frmMaiorouMenorqueDez";
+            this.Text = "Maior ou Menor que Dez";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaiorouMenorqueDez;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ maioroumenorquedez/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maioroumenorquedez
+{
+    public partial class frmMaiorouMenorqueDez : Form
+    {
+        public frmMaiorouMenorqueDez()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int Valor = (int.Parse(txtMaiorouMenorqueDez.Text));
+
+            if (Valor > 10)
+            {
+                MessageBox.Show("O valor é maior que 10", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (Valor < 10)
+            {
+                MessageBox.Show("O valor é menor que 10", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if ( Valor == 10)
+            {
+                MessageBox.Show("O valor é igual a 10", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else 
+            {
+                MessageBox.Show("Por favor, digite um número válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+
+
+
+        }
+    } 
+}
